bda79ab7ebaf4b3b41e42153c2c2855cb7c6985f
Burson555 <by570@nyu.edu>
2018-12-03 21:31:06 +0800

    added sample code and finished most web coding for final project
    as title

---
+++ communications-lab-master/w12-c24-p5-basics/p5-13-animation-random-lines/js/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 600);
+  background(0);
+}
+
+
+function draw() {
+  //background(0);
+
+  var x1 = random(width);
+  var y1 = random(height);
+  var x2 = random(width);
+  var y2 = random(height);
+
+  // noStroke();
+  stroke(random(255), random(255), random(255), random(255));
+  line(x1, y1, x2, y2);
+}
